e476159963fa02e4215e31d4f6397ce917d87412
Andrew Cutts <acutts@acgeospatial.co.uk>
2016-11-12 20:05:14 +0000

    Update Edge_Planet.py

--- Edge_Planet.py
+++ Edge_Planet.py
@@ -3,6 +3,7 @@ import os
 import cv2
 
 def edges(file):
+### runs the canny edge detection on the downloaded planet sat images
     print "file_in", file
     frame = cv2.imread(file)
     edges = cv2.Canny(frame,100,200)
@@ -10,8 +11,8 @@ def edges(file):
     fileout = name + "edges.jpg"
     cv2.imwrite(fileout, edges)
 
-client = api.Client('yourapikey')
-body = client.get_scenes_list(scene_type='ortho',count=20)
+client = api.Client('yourapikey') # you need an API key
+body = client.get_scenes_list(scene_type='ortho',count=20) # I think the count is restricted to 1000
 geojson = body.get()
 # the results are paginated and the total result set count is provided
 ls_id=[]
@@ -27,7 +28,7 @@ for f in geojson['features']:
 print "count_cloudy", count_cloudy
 print "count_non_cloudy", count_non_cloudy
 	
-directory = '...directory'
+directory = '...directory' # define the directory to save the file and later loop through
 ids = ls_id[:10]
 # create a callback that will write scenes to the 'downloads' directory
 # note - the directory must exist!
@@ -37,7 +38,7 @@ bodies = client.fetch_scene_geotiffs(ids, callback=callback)
 # any exception handling should be performed
 for b in bodies:
     b.await()
-	
+# now all the data has been downloaded you can loop through the directory and call the edge detect method	
 for filename in os.listdir(directory):
     if filename.endswith(".tif"):
         frame1 = os.path.join(directory+"/"+filename)
